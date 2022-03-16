18f4d4d5c77479a469104d9e8fb8089dc4166321
Mustodik <dawbiemusdoiq30@gmail.com>
2022-03-16 15:36:45 +0000

    first commit

---
+++ src/writers.js
@@ -0,0 +1,39 @@
+const writers=[
+    {
+    id:1,
+    name:"Jamila Lorde",
+    avatar:"audre_lorde",
+    email:"jamila23@gmail.com",
+    phone:"0550947218"
+
+    },
+    {
+        id:2,
+        name:"Ibrahim Musodiq",
+        avatar:"belle_hooks",
+        email:"ibrahih3@gmail.com",
+        phone:"0550947218"
+    },
+    {
+        id:3,
+        name:"Assad Hahim",
+        avatar:"du_bois",
+        email:"assad63@gmail.com",
+        phone:"0550947218"
+    },
+    {
+        id:4,
+        name:"Ahmed Lorde",
+        avatar:"james_baldwin",
+        email:"ahmed23@gmail.com",
+        phone:"0550947218"
+    },
+    {
+        id:5,
+        name:"Jamila Ibrahim",
+        avatar:"maya_angelou",
+        email:"jamila23@gmail.com",
+        phone:"0550947218"
+    }
+]
+export default writers;
